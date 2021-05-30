75f974576e2e2ca0f3e0949b0aa4648383ccd62c
GaryRoberts <garyroberts1995@gmail.com>
2021-05-30 18:53:27 -0500

    updates

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: nodemon src/server.js
+web: node src/server.js
--- public/index.js
+++ public/index.js
@@ -8,7 +8,7 @@ const peer = new RTCPeerConnection({
 });
 
 // Connecting to socket
-const socket = io('http://localhost:5000');
+const socket = io('http://localhost:3000');
 
 const onSocketConnected = async () => {
   const constraints = {
