84ef6496a34504f67f55aaa800a159f4f4c314e2
GaryRoberts <garyroberts1995@gmail.com>
2021-05-30 18:48:54 -0500

    update

--- public/index.js
+++ public/index.js
@@ -8,7 +8,7 @@ const peer = new RTCPeerConnection({
 });
 
 // Connecting to socket
-const socket = io('https://video-chat-app-project.herokuapp.com:3000');
+const socket = io('http://localhost:5000');
 
 const onSocketConnected = async () => {
   const constraints = {
