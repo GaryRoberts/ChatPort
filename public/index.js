107e8ed89781b55fb6264dd2bc38738b775062bc
GaryRoberts <garyroberts1995@gmail.com>
2021-05-30 20:50:25 -0500

    updates

--- public/index.js
+++ public/index.js
@@ -9,7 +9,7 @@ const peer = new RTCPeerConnection({
 
 
 const PORT = process.env.PORT || 3000;
-const INDEX = 'public/index.html';
+const INDEX = '/public/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
