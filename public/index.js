bf0fcf290c1c0a5a341c99c85686dfacbafc477f
GaryRoberts <garyroberts1995@gmail.com>
2021-05-30 22:38:51 -0500

    updates

--- public/index.js
+++ public/index.js
@@ -11,7 +11,6 @@ const peer = new RTCPeerConnection({
 
 const path = require('path');
 const PORT = process.env.PORT || 3000;
-const INDEX = '/public/index.html';
 
 const server = express()
 .use((req, res) => res.sendFile(path.resolve(process.cwd(), 'public/index.html')))
--- src/server.js
+++ src/server.js
@@ -5,10 +5,9 @@ const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/public/index.html';
 
 const server = express()
-.use((req, res) => res.sendFile(path.resolve(process.cwd(), 'public/index.html')))
+.use((req, res) => res.sendFile(path.resolve(process.cwd(), 'public')))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
